f4c19a9dd5b4606234587ed5c3d09a493581b050
Anna Luisa Patiño West <anna.luisa.west@formidablelabs.com>
2017-02-08 10:38:59 -0800

    wrap async assertions in try/catch
    restore in afterEach
    fix a few invalid assertions

--- test/lib/dashboard-agent.spec.js
+++ test/lib/dashboard-agent.spec.js
@@ -7,15 +7,18 @@ var SocketIO = require("socket.io");
 var config = require("../../lib/config");
 var dashboardAgent = require("../../lib/dashboard-agent");
 var pusage = require("pidusage");
+var tryCatch = require("../utils").tryCatch;
 
 describe("dashboard-agent", function () {
 
+  var sandbox;
   var server;
   var agent;
   var TEST_PORT = 12345;
   var MAX_EVENT_LOOP_DELAY = 10;
 
   before(function () {
+    sandbox = sinon.sandbox.create();
     process.env[config.PORT_KEY] = TEST_PORT;
     process.env[config.BLOCKED_THRESHOLD_KEY] = 1;
     process.env[config.REFRESH_INTERVAL_KEY] = 10;
@@ -26,26 +29,32 @@ describe("dashboard-agent", function () {
     server = new SocketIO(TEST_PORT);
   });
 
-  afterEach(function () {
-    server.close();
+  afterEach(function (done) {
     agent.destroy();
+    sandbox.restore();
+    server.close(done);
   });
 
   describe("initialization", function () {
 
     it("should use environment variables for configuration", function (done) {
       var checkMetrics = function (metrics) {
-        expect(metrics).to.be.exist;
+        expect(metrics).to.be.an("object");
         expect(metrics.eventLoop.delay).to.be.at.most(MAX_EVENT_LOOP_DELAY);
       };
 
       server.on("connection", function (socket) {
-        expect(socket).to.be.defined;
-        socket.on("error", done);
-        socket.on("metrics", function (data) { //eslint-disable-line max-nested-callbacks
-          socket.removeAllListeners("metrics");
-          checkMetrics(JSON.parse(data));
-          done();
+        try {
+          expect(socket).to.be.an("object");
+          socket.on("error", done);
+        } catch (err) {
+          done(err);
+        }
+        socket.on("metrics", function (data) {
+          tryCatch(done, function () {
+            socket.removeAllListeners("metrics");
+            checkMetrics(JSON.parse(data));
+          });
         });
       });
     });
@@ -55,9 +64,8 @@ describe("dashboard-agent", function () {
     it("should provide basic metrics", function (done) {
 
       var checkMetrics = function (metrics) {
-        expect(metrics).to.be.defined;
+        expect(metrics).to.be.an("object");
         expect(metrics.eventLoop).to.deep.equal({ delay: 0, high: 0 });
-        expect(metrics.mem).to.exist;
         expect(metrics.mem.systemTotal).to.be.above(0);
         expect(metrics.mem.rss).to.be.above(0);
         expect(metrics.mem.heapTotal).to.be.above(0);
@@ -66,43 +74,44 @@ describe("dashboard-agent", function () {
       };
 
       agent._getStats(function (err, metrics) {
-        expect(err).to.be.null;
-        checkMetrics(metrics);
-        done();
+        tryCatch(done, function () {
+          expect(err).to.be.null;
+          checkMetrics(metrics);
+        });
       });
     });
 
     it("should report an event loop delay and cpu stats", function (done) {
       var delay = { current: 100, max: 150 };
       var pusageResults = { cpu: 50 };
-      var pidStub = sinon.stub(pusage, "stat").yields(null, pusageResults);
+      sandbox.stub(pusage, "stat").yields(null, pusageResults);
 
       agent._delayed(delay.max);
       agent._delayed(delay.current);
 
       var checkMetrics = function (metrics) {
         expect(metrics.eventLoop.delay).to.equal(delay.current);
-        expect(metrics.eventLoop.high).to.be.equal(delay.max);
+        expect(metrics.eventLoop.high).to.equal(delay.max);
         expect(metrics.cpu.utilization).to.equal(pusageResults.cpu);
       };
 
       agent._getStats(function (err, metrics) {
-        expect(err).to.be.null;
-        checkMetrics(metrics);
-        pidStub.restore();
-        done();
+        tryCatch(done, function () {
+          expect(err).to.be.null;
+          checkMetrics(metrics);
+        });
       });
     });
 
     it("should return an error when pusage fails", function (done) {
-      var pidStub = sinon.stub(pusage, "stat").yields(new Error("bad error"));
+      sandbox.stub(pusage, "stat").yields(new Error("bad error"));
 
       agent._getStats(function (err, metrics) {
-        expect(err).to.exist;
-        expect(metrics).to.be.undefined;
-        expect(err.message).to.equal("bad error");
-        pidStub.restore();
-        done();
+        tryCatch(done, function () {
+          expect(err).to.exist;
+          expect(metrics).to.be.undefined;
+          expect(err.message).to.equal("bad error");
+        });
       });
     });
   });
